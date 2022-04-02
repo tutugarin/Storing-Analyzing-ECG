f911cae610eee9f748011b7aeede33e1e3f6c978
Yaroslav Aksenov <yaroslav.o.aksenov@gmail.com>
2022-04-02 23:11:55 +0300

    add requirements for linter and add some db code

--- analyse/utils/download_db.py
+++ analyse/utils/download_db.py
@@ -1,7 +1,9 @@
 """
-    Create list of ECG sygnals from open source db
+    Create list of ECG signals from open source db
     (e.g. from open source MIT-BIH Atrial Fibrillation Database
     https://physionet.org/content/afdb/1.0.0/)
 """
 
 import wfdb
+record = wfdb.rdrecord('Storing-Analyzing-ECG/analyse/data/recordings/04015', sampto=3000)
+annotation = wfdb.rdann('Storing-Analyzing-ECG/analyse/data/recordings/04015', 'atr', sampto=3000)
