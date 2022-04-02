e300b0da9bab84444d611e33c12b043db9c1c325
Ivan Ershov <vanya.ershoff@gmail.com>
2022-04-02 17:23:42 +0300

    Add folder for data

--- analize/utils/download_db.py
+++ analize/utils/download_db.py
@@ -1,5 +1,5 @@
 """
-    Create list of ECG sygnals
-    from open source MIT-BIH Atrial Fibrillation Database
-    (https://physionet.org/content/afdb/1.0.0/)
+    Create list of ECG sygnals from open source db
+    (e.g. from open source MIT-BIH Atrial Fibrillation Database
+    https://physionet.org/content/afdb/1.0.0/)
 """
