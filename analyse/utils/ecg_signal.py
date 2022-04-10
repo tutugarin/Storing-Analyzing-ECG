1ce00e4a39f53d00f5ebd49a49eace4487d159b4
Ivan Ershov <vanya.ershoff@gmail.com>
2022-04-10 12:15:35 +0300

    - add class Sygnal

--- analyse/utils/ecg_signal.py
+++ analyse/utils/ecg_signal.py
@@ -1,3 +1,14 @@
 """
     ECG sygnal class is defined here
 """
+
+class Sygnal:
+    def __init__(self):
+        self.data = None
+        self.windows = None
+
+    def split(self):
+        pass
+
+    def get_rr_intervals(self):
+        pass
