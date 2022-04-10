6ffbfa0b74a380f64b1d642d9d184d9380db78ff
Ivan Ershov <vanya.ershoff@gmail.com>
2022-04-10 12:20:34 +0300

    - add doc strings to Sygnal methods

--- analyse/utils/ecg_signal.py
+++ analyse/utils/ecg_signal.py
@@ -3,12 +3,22 @@
 """
 
 class Sygnal:
-    def __init__(self):
-        self.data = None
+    def __init__(self, data):
+        """
+            Initialize object with sygnal from database
+        """
+        self.data = data
         self.windows = None
 
-    def split(self):
+    def split(self, count):
+        """
+            Sptilt sygnal to windows with 'count' RR intervals
+        """
+        count=count
         pass
 
-    def get_rr_intervals(self):
+    def get_r_peaks(self):
+        """
+            Returns indecis of R peaks
+        """
         pass
